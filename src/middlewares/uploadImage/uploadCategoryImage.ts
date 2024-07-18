3aedd06cd926dec406c351c42be7b9d00a74cbbe
ilyess.torki.v <ilyess.torki.v@gmail.com>
2024-07-18 14:30:13 +0000

    update category upload image

--- src/middlewares/uploadImage/uploadCategoryImage.ts
+++ src/middlewares/uploadImage/uploadCategoryImage.ts
@@ -1,25 +1,18 @@
-import sharp from "sharp";
-import { v4 as uuidv4 } from "uuid";
-import asyncHandler from "express-async-handler";
 import { uploadSingleImage } from "./uploadImage";
+import asyncHandler from "express-async-handler";
+import { Request, Response, NextFunction } from "express";
 
-// Upload single image
+import { customCloudinary } from "../../utils/uploadToCloudinary";
+
 export const uploadCategoryImage = uploadSingleImage("image");
 
-// Image processing
-export const resizeCategoryImage = asyncHandler(async (req, res, next) => {
-  const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
+export const resizeCategoryImage = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.file) {
+      const result = await customCloudinary(req.file.buffer);
+      req.body.image = result.secure_url;
+    }
 
-  if (req.file) {
-    await sharp(req.file.buffer)
-      .resize(600, 600)
-      .toFormat("jpeg")
-      .jpeg({ quality: 95 })
-      .toFile(`uploads/categories/${filename}`);
-
-    // Save image into our db
-    req.body.image = filename;
+    next();
   }
-
-  next();
-});
+);
