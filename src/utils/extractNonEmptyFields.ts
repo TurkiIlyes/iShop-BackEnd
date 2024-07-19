ce83bd3997f582fb4ac8627ec6ae5e21e3972f9b
ilyess.torki.v <ilyess.torki.v@gmail.com>
2024-07-19 17:03:12 +0000

    fix address parsing into object

--- src/utils/extractNonEmptyFields.ts
+++ src/utils/extractNonEmptyFields.ts
@@ -1,13 +1,16 @@
-const  extractNonEmptyFields =<T>(requestBody: any, model: any): Partial<T> =>{
-    const notEmptyData: Partial<T> = {};
-  
-    for (const key of Object.keys(model.schema.paths)) {
-      if (requestBody[key]) {
-        notEmptyData[key] = requestBody[key];
-      }
+const extractNonEmptyFields = <T>(requestBody: any, model: any): Partial<T> => {
+  const notEmptyData: Partial<T> = {};
+
+  for (const key of Object.keys(model.schema.paths)) {
+    if (requestBody[key]) {
+      notEmptyData[key] = requestBody[key];
     }
-  
-    return notEmptyData;
   }
+  console.log("//");
+  console.log(requestBody);
+  console.log(notEmptyData);
+  console.log("//");
+  return notEmptyData;
+};
 
 export default extractNonEmptyFields;
